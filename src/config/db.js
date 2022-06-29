4406b635bb4cd347def28aafe369e16e0d2f543e
saul <saulflores3126@gmail.com>
2022-06-29 14:34:54 -0600

    updated database name

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool, Client } = require('pg')
 const pool = new Pool({
     user: 'postgres',
     host: '172.17.0.2',
-    database: 'budget_app',
+    database: 'postgres',
     password: 's.flores9',
     port: 5432,
 })
