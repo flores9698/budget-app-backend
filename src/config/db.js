9a155529bb1f2207c0a309e3fad86fe3e585cdb1
saul <saulflores3126@gmail.com>
2022-06-29 21:01:24 -0600

    renamed host for database

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const { Pool, Client } = require('pg')
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'database',
     database: 'postgres',
     password: 's.flores9',
     port: 5433,
