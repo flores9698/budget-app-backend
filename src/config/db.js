9af3aca0f251cfa9bfa93f7251d39fdf90789a23
saul <saulflores3126@gmail.com>
2022-06-30 01:44:44 -0600

    ignore

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const { Pool, Client } = require('pg')
 const pool = new Pool({
     user: 'postgres',
-    host: 'database',
+    host: '35.239.223.48',
     database: 'budget_app',
     password: 's.flores9',
     port: 5432,
